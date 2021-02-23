46dd720812369ae8680487da944de92d9d4f83b5
David Bernadi <davidbernadi13@gmail.com>
2021-02-23 17:38:04 +0700

    simple worker example

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
-
-	"github.com/david1312/nendou-messenger/api"
-	"github.com/david1312/nendou-messenger/db"
 )
 
 var (
@@ -20,30 +13,30 @@ var (
 	RedisAddr  = "localhost:6379"
 )
 
-func main() {
-	listener, err := net.Listen("tcp", ListenAddr)
-	if err != nil {
-		log.Fatalf("Error occurred: %s", err.Error())
-	}
-	database, err := db.NewDatabase(RedisAddr)
-	if err != nil {
-		log.Fatalf("Failed to connect to redis: %s", err.Error())
-	}
+// func main() {
+// 	listener, err := net.Listen("tcp", ListenAddr)
+// 	if err != nil {
+// 		log.Fatalf("Error occurred: %s", err.Error())
+// 	}
+// 	database, err := db.NewDatabase(RedisAddr)
+// 	if err != nil {
+// 		log.Fatalf("Failed to connect to redis: %s", err.Error())
+// 	}
 
-	httpHandler := api.NewHandler(database)
-	server := &http.Server{
-		Handler: httpHandler,
-	}
-	go func() {
-		server.Serve(listener)
-	}()
-	defer Stop(server)
-	log.Printf("Started server on %s", ListenAddr)
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	log.Println(fmt.Sprint(<-ch))
-	log.Println("Stopping API server.")
-}
+// 	httpHandler := api.NewHandler(database)
+// 	server := &http.Server{
+// 		Handler: httpHandler,
+// 	}
+// 	go func() {
+// 		server.Serve(listener)
+// 	}()
+// 	defer Stop(server)
+// 	log.Printf("Started server on %s", ListenAddr)
+// 	ch := make(chan os.Signal, 1)
+// 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+// 	log.Println(fmt.Sprint(<-ch))
+// 	log.Println("Stopping API server.")
+// }
 
 func Stop(server *http.Server) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
